b5928ee418a5d5bf29a18cb4c96f820624bdaa4c
Frederic MEZOU <frederic.mezou@free.fr>
2015-11-15 18:48:06 +0100

    Add a dump and a load method for Product Class, and switch the catalog database from ini to JSON format

--- appdownload/cots/core.py
+++ appdownload/cots/core.py
@@ -27,8 +27,7 @@ class BaseProduct:
         name: is the name of the application as it appears in the Program
           Control Panel.
         version: is the current version of the product
-        published: is the date of the installer’s publication
-          (in ISO 8601 format)
+        published: is the date of the installer’s publication (ISO 8601 format)
         target: is the target architecture type (the Windows’ one) for the
           application. This argument must be one of the following values:
           'x86', 'x64' or 'unified'.
@@ -40,7 +39,7 @@ class BaseProduct:
           application.
         installer: filename of the installer (local full path)
         std_inst_args: arguments to do a standard installation.
-        silent_inst_arg: arguments to do a silent installation.
+        silent_inst_args: arguments to do a silent installation.
         update_available: is a flag indicating if a new version is available
           or not.
         update_version: is the version of the last release of the product.
@@ -60,11 +59,11 @@ class BaseProduct:
         fetch_update: downloads the latest version of the installer
     """
 
-    def __init__(self):
+    def __init__(self, logger=logging.getLogger(__name__)):
         """Constructor
 
         Parameters
-            None
+            logger : is a logger object
         """
         self.id = None
         self.name = ""
@@ -74,7 +73,7 @@ class BaseProduct:
         self.release_note = ""
         self.installer = ""
         self.std_inst_args = ""
-        self.silent_inst_arg = ""
+        self.silent_inst_args = ""
         self.update_available = False
         self.update_version = ""
         self.update_published = ""
@@ -83,7 +82,68 @@ class BaseProduct:
         self._catalog_location = ""
         self._product_code = ""
 
-        self.logger = None
+        # check logger parameter
+        self._logger = logger
+        if not isinstance(logger, logging.Logger):
+            msg = "logger argument must be a class 'logging.Logger'. not {0}"
+            msg = msg.format(logger.__class__)
+            raise TypeError(msg)
+        # To make the module as versatile as possible, an nullHandler is added.
+        # see 'Configuring Logging for a Library'
+        # docs.python.org/3/howto/logging.html#configuring-logging-for-a-library
+        self._logger.addHandler(logging.NullHandler())
+        self._logger.debug("Instance created.")
+
+    def load(self, attributes=None):
+        # attrib
+        """Load a product class.
+
+        Parameters
+            attributes: is a dictionary object containing the instance
+            variables values.
+            If attributes is not present or have the None value, instance
+            variables keep to their default values.
+            Key value pairs which don't exist in the instance variables
+            dictionary are ignored.
+        """
+        if attributes is not None:
+            # check parameters type
+            if not isinstance(attributes, dict):
+                msg = "props argument must be a class 'dict'. not {0}"
+                msg = msg.format(attributes.__class__)
+                raise TypeError(msg)
+
+        # set instance variables
+        self._logger.info("Load the product.")
+        for k,v in self.__dict__.items():
+            if k.startswith('_'):
+                continue # non-public instance variables are ignored
+            else:
+                attr = attributes.get(k)
+                if attr is not None:
+                    self.__dict__[k]=attributes.get(k)
+                    msg = "Instance variables '{0}' : " \
+                          "'{1}' -> '{2}'".format(k, v, attr)
+                    self._logger.debug(msg)
+
+    def dump(self):
+        """Dump a product class.
+
+        Parameters
+            None
+
+        Return
+            attributes: is a dictionary object containing a copy of the instance
+            variables values.
+        """
+        attributes = {}
+        self._logger.info("Dump the product.")
+        for k,v in self.__dict__.items():
+            if k.startswith('_'):
+                continue  # non-public instance variables are ignored
+            else:
+                attributes[k] = v
+        return attributes
 
     def check_update(self):
         """checks if a new version is available
@@ -100,4 +160,3 @@ class BaseProduct:
             path: is the path name where to store the installer package.
         """
         raise NotImplementedError
-
