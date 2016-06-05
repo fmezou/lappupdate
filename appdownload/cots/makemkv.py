ddec912364b3f591eee6a3fa84dd43fca8ffac6f
Frederic MEZOU <frederic.mezou@free.fr>
2016-06-05 19:14:51 +0200

    Add a release note extractor

--- appdownload/cots/makemkv.py
+++ appdownload/cots/makemkv.py
@@ -18,6 +18,9 @@ This module has only one public class.
 
 import datetime
 import logging
+import re
+from html.parser import HTMLParser
+
 
 from cots import core
 from cots import pad
@@ -291,6 +294,20 @@ class Product(core.BaseProduct):
         Extract the release note’s URL from the PAD File.
         """
         self.release_note = "http://www.makemkv.com/download/history.html"
+
+        # url="http://www.makemkv.com/download/history.html"
+        # local_filename = core.retrieve_tempfile(url)
+        local_filename = r".\MakeMKV - Revision history.html"
+        # print("History downloaded: '{0}'".format(local_filename))
+
+        parser = MyHTMLParser()
+
+        with open(local_filename) as file:
+            parser.feed(file.read())
+
+        # os.unlink(local_filename)
+
+
         msg = "Release note :'{0}'"
         _logger.debug(msg.format(self.release_note))
 
@@ -309,3 +326,473 @@ class Product(core.BaseProduct):
         self.silent_inst_args = "/S"
         msg = "Silent installation option :'{0}'"
         _logger.debug(msg.format(self.silent_inst_args))
+
+
+# ex: MakeMKV v1.9.7 ( 5.10.2015 )
+# MakeMKV v1.01 build 646
+_title_re = re.compile("^MakeMKV v(?P<version>(([0-9]+\.)+([0-9]+)))"
+                       "((\s+\(\s*(?P<date>(([0-9]+\.)+([0-9]+)))\s*\))?"
+                       "|(\s+build\s+(?P<build>[0-9]+))?)$")
+
+
+class MyHTMLParser(HTMLParser):
+    """
+    Null
+     |
+     | <div id="content">
+     v
+    Content --------------------
+     |                         |
+     | <ul class="bullets">    | </div>
+     v                         v
+    Title -------              Null
+     |          |
+     | <li>     | </ul>
+     |          v
+     v          Content
+    Release ---------
+     |              |
+     | _title_re    | !title_re
+     v              v
+    Notes          Ignore
+     |              |
+     |              | <ul class="bullets2">
+     |              v
+                    NotRecording ----------------------------------------
+                     |                     |                          |
+                     | </ul> and _ul=0     | <ul>                     | </ul> and _ul!=0
+                     v                     v                          v
+                     Title              NotRecording _ul+=1           NotRecording _ul-=1
+     |
+     |
+     |
+     |
+     | <ul class="bullets2">
+     v
+    Recording ----------------------------------------
+     |                     |                          |
+     | </ul> and _ul=0     | <ul>                     | </ul> and _ul!=0
+     v                     v                          v
+     Title              Recording _ul+=1             Recording _ul-=1
+    """
+    # Scheduler's state
+    _STATE_NULL = 0
+    _STATE_CONTENT = 1
+    _STATE_RELEASES_LIST = 2
+    _STATE_RELEASE_ID = 3
+    _STATE_RELEASE_NOTES = 4
+    _STATE_FETCHING = 5
+    _STATE_IGNORE =6
+    _STATE_NOT_RECORDING = 7
+
+    # Events
+    _START_TAG_EVT = 0
+    _END_TAG_EVT = 1
+    _DATA_EVT = 2
+
+    # todo : make the automate graph considering the approved version (capture delta)
+
+    def __init__(self):
+        super().__init__()
+
+        self._state = self._STATE_NULL
+        self._ul_count = 0
+
+        self.note = ""
+
+        self._sched_map = {
+            self._STATE_NULL: [
+                self._null_actuating, [
+                    (self._is_content_beginning, self._STATE_CONTENT)
+                ]
+            ],
+            self._STATE_CONTENT: [
+                self._null_actuating, [
+                    (self._is_releases_list_beginning, self._STATE_RELEASES_LIST),
+                    (self._is_content_ending, self._STATE_NULL)
+                ]
+            ],
+            self._STATE_RELEASES_LIST: [
+                self._null_actuating, [
+                    (self._is_releases_list_ending, self._STATE_CONTENT),
+                    (self._is_release_id_beginning, self._STATE_RELEASE_ID)
+                ]
+            ],
+            self._STATE_RELEASE_ID: [
+                self._release_id_fetching, [
+                    (self._is_expected_release, self._STATE_RELEASE_NOTES),
+                    (self._is_unknown_release, self._STATE_IGNORE)
+                ]
+            ],
+            self._STATE_RELEASE_NOTES: [
+                self._null_actuating, [
+                    (self._is_release_notes_beginning, self._STATE_FETCHING),
+                ]
+            ],
+            self._STATE_FETCHING: [
+                self._release_notes_fetching, [
+                    (self._is_release_notes_ending, self._STATE_RELEASES_LIST),
+                ]
+            ],
+            self._STATE_IGNORE: [
+                self._null_actuating, [
+                    (self._is_release_notes_beginning, self._STATE_IGNORE),
+                    (self._is_release_notes_ending, self._STATE_RELEASES_LIST),
+                ]
+            ]
+        }
+        self._actuating = None
+        self._transitions = None
+        self._set_state(self._state)
+
+        self._release = ""
+        self._release_notes = ""
+        self._tag_depth = 0
+        self._depth_map = {}
+        self._release_id = None
+
+    def _process_event(self, event, data, attributes):
+        """
+        Process the event
+
+        Args:
+            event (int): The event identifier
+            data (str): The tag identifier (i.e. ul) or the text.
+            attributes (dict):  The attributes of the tag.
+        """
+        if event == self._START_TAG_EVT:
+            self._tag_depth += 1
+
+        for transition, state in self._transitions:
+            if transition(event, data, attributes):
+                self._actuating(event, data, attributes, first=False, last=True)
+                self._set_state(state)
+                self._actuating(event, data, attributes, first=True, last=False)
+                break
+        else:
+            self._actuating(event, data, attributes)  # no transition
+
+        if event == self._END_TAG_EVT:
+            self._tag_depth -= 1
+
+    def _set_state(self, state):
+        """
+        Set the state
+
+        Args:
+            state (int): The state identifier.
+        """
+        print("State {} -> {}".format(self._state, state))
+        self._actuating = self._sched_map[state][0]
+        self._transitions = self._sched_map[state][1]
+        self._state = state
+
+    def _null_actuating(self, event, data, attributes,
+                        first=False, last=False):
+        """
+        Null state actuating.
+
+        Args:
+            event (int): The event identifier
+            data (str): The tag identifier (i.e. ul) or the text.
+            attributes (dict):  The attributes of the tag.
+            first (boolean, optional): Indicate if it's the first call of the
+            actuating function (i.e. just after a state change (see
+            `_set_state`) including if it's the same state). False is the
+            default.
+            last (boolean, optional): Indicate if it's the last call of the
+            actuating function (i.e. just before a state change (see
+            `_set_state`) including if it's the same state). False is the
+            default.
+        """
+        pass
+
+    def _is_content_beginning(self, event, data, attributes):
+        """
+
+        Args:
+            event (int): The event identifier
+            data (str): The tag identifier (i.e. ul) or the text.
+            attributes:  The attributes of the tag.
+
+        Returns:
+            bool: True if the transition is verified.
+        """
+        verified = False
+
+        if event == self._START_TAG_EVT and data == "div":
+            if "id" in attributes:
+                if attributes["id"] == "content":
+                    verified = True
+                    self._depth_map["content"] = self._tag_depth
+
+        return verified
+
+    def _is_content_ending(self, event, data, attributes):
+        """
+
+        Args:
+            event (int): The event identifier
+            data (str): The tag identifier (i.e. ul) or the text.
+            attributes:  The attributes of the tag.
+
+        Returns:
+            bool: True if the transition is verified.
+        """
+        verified = False
+
+        if event == self._END_TAG_EVT and data == "div":
+            if self._depth_map["content"] == self._tag_depth:
+                verified = True
+
+        return verified
+
+    def _is_releases_list_beginning(self, event, data, attributes):
+        """
+
+        Args:
+            event (int): The event identifier
+            data (str): The tag identifier (i.e. ul) or the text.
+            attributes:  The attributes of the tag.
+
+        Returns:
+            bool: True if the transition is verified.
+        """
+        verified = False
+
+        if event == self._START_TAG_EVT and data == "ul":
+            if "class" in attributes:
+                if attributes["class"] == "bullets":
+                    verified = True
+                    self._depth_map["release_list"] = self._tag_depth
+
+        return verified
+
+    def _is_releases_list_ending(self, event, data, attributes):
+        """
+
+        Args:
+            event (int): The event identifier
+            data (str): The tag identifier (i.e. ul) or the text.
+            attributes:  The attributes of the tag.
+
+        Returns:
+            bool: True if the transition is verified.
+        """
+        verified = False
+
+        if event == self._END_TAG_EVT and data == "ul":
+            if self._depth_map["release_list"] == self._tag_depth:
+                verified = True
+
+        return verified
+
+    def _is_release_id_beginning(self, event, data, attributes):
+        """
+
+        Args:
+            event (int): The event identifier
+            data (str): The tag identifier (i.e. ul) or the text.
+            attributes:  The attributes of the tag.
+
+        Returns:
+            bool: True if the transition is verified.
+        """
+        verified = False
+
+        if event == self._START_TAG_EVT and data == "li":
+            verified = True
+            self._depth_map["release_id"] = self._tag_depth
+            self._release = ""
+
+        return verified
+
+    def _release_id_fetching(self, event, data, attributes,
+                             first=False, last=False):
+        """
+        Release state actuating.
+
+        Args:
+            event (int): The event identifier
+            data (str): The tag identifier (i.e. ul) or the text.
+            attributes (dict):  The attributes of the tag.
+            first (boolean, optional): Indicate if it's the first call of the
+            actuating function (i.e. just after a state change (see
+            `_set_state`) including if it's the same state). False is the
+            default.
+            last (boolean, optional): Indicate if it's the last call of the
+            actuating function (i.e. just before a state change (see
+            `_set_state`) including if it's the same state). False is the
+            default.
+        """
+        if first:
+            self._release = ""
+
+        if event == self._DATA_EVT:
+            self._release += data
+
+        if last:
+            if self._release_id:
+                if self._release_id.group("date"):
+                    pubdate = self._release_id.group("date").split(".")
+                    dt = datetime.date(int(pubdate[2]), int(pubdate[1]),
+                                       int(pubdate[0]))
+                else:
+                    dt = None
+                ver_id = self._release_id.group("version").split(".")
+                ver = "{}.{}.0".format(int(ver_id[0]), int(ver_id[1]))
+                if len(ver_id) == 3:
+                    ver = "{}.{}.{}".format(int(ver_id[0]), int(ver_id[1]),
+                                            int(ver_id[2]))
+                if self._release_id.group("build"):
+                    ver += "+{}".format(self._release_id.group("build"))
+                version = semver.SemVer(ver)
+                print("MakeMKV", version, dt)
+            else:
+                print("ERROR Unknown MakeMKV")
+
+    def _is_expected_release(self, event, data, attributes):
+        """
+
+        Args:
+            event (int): The event identifier
+            data (str): The tag identifier (i.e. ul) or the text.
+            attributes:  The attributes of the tag.
+
+        Returns:
+            bool: True if the transition is verified.
+        """
+        verified = False
+
+        if event == self._END_TAG_EVT and data == "li":
+            if self._depth_map["release_id"] == self._tag_depth:
+                self._release_id = _title_re.match(self._release)
+                if self._release_id:
+                    verified = True
+
+        return verified
+
+    def _is_unknown_release(self, event, data, attributes):
+        """
+
+        Args:
+            event (int): The event identifier
+            data (str): The tag identifier (i.e. ul) or the text.
+            attributes:  The attributes of the tag.
+
+        Returns:
+            bool: True if the transition is verified.
+        """
+        verified = False
+
+        if event == self._END_TAG_EVT and data == "li":
+            if self._depth_map["release_id"] == self._tag_depth:
+                self._release_id = _title_re.match(self._release)
+                if not self._release_id:
+                    verified = True
+
+        return verified
+
+    def _is_release_notes_beginning(self, event, data, attributes):
+        """
+
+        Args:
+            event (int): The event identifier
+            data (str): The tag identifier (i.e. ul) or the text.
+            attributes:  The attributes of the tag.
+
+        Returns:
+            bool: True if the transition is verified.
+        """
+        verified = False
+
+        if event == self._START_TAG_EVT and data == "ul":
+            if "class" in attributes:
+                if attributes["class"] == "bullets2":
+                    verified = True
+                    self._depth_map["release_notes"] = self._tag_depth
+
+        return verified
+
+    def _release_notes_fetching(self, event, data, attributes,
+                                first=False, last=False):
+        """
+        Release notes state actuating.
+
+        Args:
+            event (int): The event identifier
+            data (str): The tag identifier (i.e. ul) or the text.
+            attributes (dict):  The attributes of the tag.
+            first (boolean, optional): Indicate if it's the first call of the
+            actuating function (i.e. just after a state change (see
+            `_set_state`) including if it's the same state). False is the
+            default.
+            last (boolean, optional): Indicate if it's the last call of the
+            actuating function (i.e. just before a state change (see
+            `_set_state`) including if it's the same state). False is the
+            default.
+        """
+        if first:
+            self._release_notes = ""
+
+        if event == self._DATA_EVT:
+            self._release_notes += data
+        elif event == self._START_TAG_EVT:
+            self._release_notes += "<{}>".format(data)
+        elif event == self._END_TAG_EVT:
+            self._release_notes += "</{}>".format(data)
+
+        if last:
+            print(self._release_notes)
+
+    def _is_release_notes_ending(self, event, data, attributes):
+        """
+
+        Args:
+            event (int): The event identifier
+            data (str): The tag identifier (i.e. ul) or the text.
+            attributes:  The attributes of the tag.
+
+        Returns:
+            bool: True if the transition is verified.
+        """
+        verified = False
+
+        if event == self._END_TAG_EVT and data == "ul":
+            if self._depth_map["release_notes"] == self._tag_depth:
+                verified = True
+
+        return verified
+
+    def handle_starttag(self, tag, attrs):
+        """
+        https://docs.python.org/3/library/html.parser.html#html.parser.HTMLParser.handle_starttag
+        """
+        attributes = {}
+        for attr in attrs:
+            attributes[attr[0]] = attr[1]
+        self._process_event(self._START_TAG_EVT, tag, attributes)
+
+    def handle_endtag(self, tag):
+        self._process_event(self._END_TAG_EVT, tag, None)
+
+    def handle_data(self, data):
+        self._process_event(self._DATA_EVT, data, None)
+
+    # def handle_comment(self, data):
+    #     print("Comment  :", data)
+    #
+    # def handle_entityref(self, name):
+    #     c = chr(name2codepoint[name])
+    #     print("Named ent:", c)
+    #
+    # def handle_charref(self, name):
+    #     if name.startswith('x'):
+    #         c = chr(int(name[1:], 16))
+    #     else:
+    #         c = chr(int(name))
+    #     print("Num ent  :", c)
+    #
+    # def handle_decl(self, data):
+    #     print("Decl     :", data)
+
